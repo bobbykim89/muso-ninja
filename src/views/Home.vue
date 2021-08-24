e18bf3573aee8dda2b40231143ad06b7d232a5bc
bobbykim89 <bobby.sihun.kim@gmail.com>
2021-08-24 15:53:19 -0700

    createplaylist, listview, useStorage added

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,25 @@
 <template>
   <div class="home">
-    <p>HomePage</p>
+    <div v-if="error" class="error">
+      {{ error }}
+    </div>
+    <div v-if="documents">
+      <ListView :playlists="documents" />
+    </div>
   </div>
 </template>
 
 <script>
+import getCollection from '@/composables/getCollection';
+import ListView from '../components/ListView.vue';
+
 export default {
   name: 'Home',
-  components: {},
+  components: { ListView },
+  setup() {
+    const { documents, error } = getCollection('playlists');
+
+    return { documents, error };
+  },
 };
 </script>
---
+++ src/views/auth/Signup.vue
@@ -0,0 +1,38 @@
+<template>
+  <form @submit.prevent="handleSubmit">
+    <h3>Sign up</h3>
+    <input type="text" placeholder="Display name" v-model="displayName" />
+    <input type="email" placeholder="Email" v-model="email" />
+    <input type="password" placeholder="Password" v-model="password" />
+    <div class="error" v-if="error">
+      {{ error }}
+    </div>
+    <button v-if="!isPending">Sign up</button>
+    <button v-if="isPending" disabled>Loading</button>
+  </form>
+</template>
+
+<script>
+import { ref } from '@vue/reactivity';
+import useSignup from '@/composables/useSignup';
+export default {
+  setup() {
+    const { error, signup, isPending } = useSignup();
+
+    const email = ref('');
+    const password = ref('');
+    const displayName = ref('');
+
+    const handleSubmit = async () => {
+      const res = await signup(email.value, password.value, displayName.value);
+      if (!error.value) {
+        console.log('User signed up!');
+      }
+    };
+
+    return { email, password, displayName, isPending, handleSubmit };
+  },
+};
+</script>
+
+<style></style>
